d6af4a3af3801f51f825d0473092cf0710da11dd
hafsaasaleem <shaikhhafsa215@gmail.com>
2023-11-05 19:09:49 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/drive_seller/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drive_seller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:drive_seller/src/features/onboarding/on_boarding_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  const MaterialApp(
+      home: OnBoardingScreen(),
+    );
+  }
+}
+
